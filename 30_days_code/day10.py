2fa7e917622d1ebd34605fd3f8abc6ba19062c6b
joseph birara <josephbirara92@gmail.com>
2021-12-16 19:47:16 +0300

    problem#10

---
+++ 30_days_code/day10.py
@@ -0,0 +1,30 @@
+# given an input enteger number , convert it in to base 2 
+# and return the maximum number of conscative 1's
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+
+    result = 0
+    maximum = 0
+
+    while n > 0:
+        if n % 2 == 1:
+            result += 1
+            if result > maximum:
+                maximum = result
+
+        else:
+            result = 0
+
+        n //= 2
+
+    print(maximum)
